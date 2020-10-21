ddcd81b6278b703e9b99113abfa119ff14b6d413
Abdullatif-Fk <abul647.fk@gmail.com>
2020-10-21 11:33:40 +0300

    step2

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const app = express();
 const PORT = 3000;
 
 
-app.get('/', (req, res) => res.send('ok'))
+/*app.get('/', (req, res) => res.send('ok'))
 
 
-app.listen(PORT ,()=>console.log(`Connected to ${PORT}`))
+*/
+app.get('/', (req, res) => {
+    return res.send('ok');
+  });
+   
+  app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+  });
+   
+  app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+  });
+   
+  app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+  });
+  app.listen(PORT ,()=>console.log(`Connected to ${PORT}`))
