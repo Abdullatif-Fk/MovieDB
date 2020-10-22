cb1daf77f0f8f52ca6bdc3921acc91bcf0a7b42c
Abdullatif-Fk <abul647.fk@gmail.com>
2020-10-22 10:03:21 +0300

    step 3

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const app = express();
 const PORT = 3000;
 
 
-/*app.get('/', (req, res) => res.send('ok'))
+app.get('/', (req, res) => res.send('ok'))
 
 
-*/
-app.get('/', (req, res) => {
-    return res.send('ok');
+app.get('/test', (req, res) => {
+    //return res.send('ok');
+
+    res.send(
+      {
+           status:200,
+           message:"ok"
+      }
+  )
+
+
+  });
+  app.get('/time', (req, res) => {
+    //return res.send('ok');
+    let date = new Date();
+    let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    res.send(
+      {status:200, message:time}
+  )
+
+  
   });
    
   app.post('/', (req, res) => {
