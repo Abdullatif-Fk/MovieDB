3f4e05c4efaf416ab5501ce4ac174cdc548606da
Abdullatif-Fk <abul647.fk@gmail.com>
2020-10-22 13:18:04 +0300

    step 5

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+
+  const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
 const express =require('express');
 const app = express();
 const PORT = 3000;
@@ -60,6 +68,16 @@ app.get('/test', (req, res) => {
     }
 
   });
+  
+  app.get('/movies/create', (req, res) => {res.send('read')})
+  app.get('/movies/read', (req, res) => {
+    //let mouvies=req.params.movies;
+    res.send({status:200, data:movies })
+  
+  })
+  app.get('/movies/update', (req, res) => {res.send('update')})
+  app.get('/movies/delete', (req, res) => {res.send('delete')})
+
 
   
 
